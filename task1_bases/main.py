087a7830106bf056958cdaeff5e34c3924cba6db
jose <joseco04@live.com>
2020-09-19 12:07:16 +0300

    script task1 working

--- task1_bases/main.py
+++ task1_bases/main.py
@@ -1,5 +1,6 @@
 import numpy as np
-import sys
+
+# import sys
 
 dic = {
     '1': [66, 32, 38, 37, 1],
@@ -7,9 +8,16 @@ dic = {
     '3': [8, 7, 61]
 }
 
-minSize = -sys.maxsize - 1
+# minSize = -sys.maxsize - 1
 for key, arr in dic.items():
     arr[0] = np.random.randint(-100, -1)
 
-print(dic)
+cumSumArr = []
+maxValuesArr = []
+for key, arr in dic.items():
+    cumSumArr.append(np.sum(arr))
+    maxValuesArr.append(np.max(arr))
 
+dic['4'] = maxValuesArr
+
+print(dic, cumSumArr)
