3237f6106d935bd6362d17629a7d3e102fe3b5d8
jose <joseco04@live.com>
2020-10-01 13:53:09 +0300

    exercise 9 working

---
+++ task2.1_panda_work_datasets/my_dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Dataset Taken from: https://www.kaggle.com/lava18/google-play-store-apps?select=googleplaystore.csv
+# Download link: https://www.kaggle.com/lava18/google-play-store-apps?resource=download/DpSwfHpLM42wPLJJQ8JZ%2Fversions%2Fb3xEnJAJii7SgRS3kQph%2Ffiles%2Fgoogleplaystore.csv&downloadHash=de08bde8dc99bd289419c0b570da2c886c2405cf77c3c5e1956b634cc5275bc8
+
+data = pd.read_csv("googleplaystore.csv")
+
+print(data)
--- task2.1_panda_work_datasets/teacher_dataset.py
+++ task2.1_panda_work_datasets/teacher_dataset.py
@@ -35,8 +35,8 @@ title5 = 'relation age-death, age > 14 over time of death using '
 
 # 6
 title6 = 'Dead people under 30 by state using '
-# data6 = data.query('age < 30 and status == "D"')
-# valuesByState = data6['state'].value_counts()
+data6 = data.query('age < 30 and status == "D"')
+valuesByState = data6['state'].value_counts()
 # line61 = valuesByState.plot.pie(y='state', startangle=90, autopct='%1.1f%%', title=title6+'pandas.plot.pie')
 # plt.show()
 
@@ -47,16 +47,23 @@ title6 = 'Dead people under 30 by state using '
 
 
 # 7
-
+data7 = data.query('status == "D"').groupby('state')['age'].mean()
+wholeAusAverage = pd.Series({"AUS": data.query('status == "D"')['age'].mean()})
+merged7 = pd.concat([data7, wholeAusAverage])
+# line7 = merged7.plot.barh(rot=0, color=['green'], title='dead people by region and whole AUS using pandas.plot.bar')
+# line7.set(xlabel='average age', ylabel='states')
+# for index, value in enumerate(merged7):
+#     plt.text(value - 5, index, str(round(value, 3)), color='white')
+# plt.show()
 
 
 # 8
 data81 = data.query('status == "D"').groupby('state')['age'].max()
-print("oldest reported age of people that died by region: \n", data81)
-data82 = data.query('status == "D"').groupby('state')['age'].min()
-print("youngest reported age of people that died by region: \n", data82)
+# print("oldest reported age of people that died by region: \n", data81)
+# data82 = data.query('status == "D"').groupby('state')['age'].min()
+# print("youngest reported age of people that died by region: \n", data82)
 
-# averages8 = data.groupby('state')['age'].mean()
+averages8 = data.groupby('state')['age'].mean()
 # print(averages8[averages8])
 # print("regions where infections average was bigger than the old people (from 55): ", 1)
 
@@ -64,7 +71,23 @@ print("youngest reported age of people that died by region: \n", data82)
 
 # print("regions where infections average was bigger than the adult people (31;54): ", 1)
 
+
 # 9
+data9 = data.groupby('state')['T.categ'].value_counts()
+# formatting for plotting (I'm not proud of this code :()
+indexes9 = set()
+dataInfected9 = {}
+for (k, v), n in data9.items():
+    indexes9.add(k)
+    dataInfected9.setdefault(v, []).append(n)
+
+plotData9 = pd.DataFrame(dataInfected9, index=indexes9)
+lines9 = plotData9.plot.bar(title='types of infections by region')
+lines9.set(xlabel='states', ylabel='amount')
+plt.show()
 
 
 # 10
+# youngPercent = 1
+# adultPercent = 1
+# oldPercent = 1
